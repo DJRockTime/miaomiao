0f59a762b93c4c284aaf4fb61a166a052f6219cb
Rock <124841882@qq.com>
2019-06-21 00:41:22 +0800

    add createComponents

---
+++ src/routers/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter,
+    {
+      path: '/*',
+      redirect: movieRouter,
+    }
+    // example:
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.  按需加载（懒加载）
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
+  ]
+})
